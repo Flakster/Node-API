704fc4d1657fec4efd419f64c49b22c020308e28
Carlos Santamaria <carlos.santamaria@gmail.com>
2024-08-05 12:14:39 -0500

    add the route for the updateUser method

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -4,10 +4,12 @@ import {
   getUser,
   getUsers,
   createUser,
+  updateUser
 } from "../controllers/user.controllers.js";
 
 router.get("/user/:id", getUser);
 router.get("/users", getUsers);
 router.post("/user", createUser);
+router.put("/user/:id", updateUser)
 
 export default router;
