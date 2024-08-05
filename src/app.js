5bb77413ccfcf533853adb77892c8c618ab43126
Carlos Santamaria <carlos.santamaria@gmail.com>
2024-08-04 19:06:08 -0500

    import and use the user.routes.js file to the app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import homeRoutes from "./routers/home.routes.js";
+import userRoutes from "./routers/user.routes.js"
 const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 app.use("/api/v1", homeRoutes);
+app.use("/api/v1", userRoutes);
 app.use((req, res) => {
   res.status(404).send("<h1>Not found :-(</h1>");
 });
