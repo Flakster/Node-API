314631419a705aec6165ac2124ae1178a5cfaffa
Carlos Santamaria <carlos.santamaria@gmail.com>
2024-08-05 08:41:59 -0500

    add express json parser

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import homeRoutes from "./routers/home.routes.js";
-import userRoutes from "./routers/user.routes.js"
-import postRoutes from "./routers/post.routes.js"
+import userRoutes from "./routers/user.routes.js";
+import postRoutes from "./routers/post.routes.js";
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
 app.use("/api/v1", homeRoutes);
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", postRoutes);
