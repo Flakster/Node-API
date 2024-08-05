3b06a63e3856fa0e38b29518a22a3098009e71b7
Carlos Santamaria <carlos.santamaria@gmail.com>
2024-08-05 08:41:24 -0500

    add a route for the createUser method

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 const router = Router();
-import { getUsers } from "../controllers/user.controllers.js";
+import { getUsers, createUser } from "../controllers/user.controllers.js";
 
 router.get("/users", getUsers);
+router.post("/user", createUser);
 
 export default router;
