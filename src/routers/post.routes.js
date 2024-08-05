21b00b57c1f10bb030d9c1aa3e32b5a1545ea877
Carlos Santamaria <carlos.santamaria@gmail.com>
2024-08-04 19:51:05 -0500

    add the routes file for posts

---
+++ src/routers/post.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { getPosts } from "../controllers/post.controllers.js";
+
+const router = Router();
+
+router.get("/posts", getPosts);
+export default router;
