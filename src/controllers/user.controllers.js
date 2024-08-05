bc3b01ff361b9fa9fbcf731879583903e857f274
Carlos Santamaria <carlos.santamaria@gmail.com>
2024-08-05 08:40:39 -0500

    created the createUser method

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -5,7 +5,18 @@ export async function getUsers(req, res) {
     const users = await User.findAll();
     res.json(users);
   } catch (error) {
-    console.log(`error: ${error}`)
-    res.status(500).json({"msg": error});
+    console.log(`error: ${error}`);
+    res.status(500).json({ msg: error.msg });
+  }
+}
+
+export async function createUser(req, res) {
+  try {
+    const { username, email } = req.body;
+    const user = User.create({ username, email });
+    res.status(201).json({ msg: "User successfully created" });
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ msg: error.msg });
   }
 }
