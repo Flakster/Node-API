e2df3129466f674cd00bf836726c13e770079c91
Carlos Santamaria <carlos.santamaria@gmail.com>
2024-08-04 19:52:18 -0500

    import and use the post routes file from the app

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import morgan from "morgan";
 import homeRoutes from "./routers/home.routes.js";
 import userRoutes from "./routers/user.routes.js"
+import postRoutes from "./routers/post.routes.js"
 const app = express();
 
 app.use(morgan("dev"));
 app.use("/api/v1", homeRoutes);
 app.use("/api/v1", userRoutes);
+app.use("/api/v1", postRoutes);
 app.use((req, res) => {
   res.status(404).send("<h1>Not found :-(</h1>");
 });
